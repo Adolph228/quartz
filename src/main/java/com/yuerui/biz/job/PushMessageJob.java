0f83cd20a5ffa898459e2a53af81c3fa9b185421
yanao <yanao1228@126.com>
2018-06-24 14:25:24 +0800

    first commit

---
+++ src/main/java/com/yuerui/biz/job/PushMessageJob.java
@@ -0,0 +1,10 @@
+package com.yuerui.biz.job;
+
+public class PushMessageJob {
+	
+	 public void execute() {
+        // 需要做的事情
+        System.out.println("执行targetObject中的targetMethod方法，开始！");
+        System.out.println("执行targetObject中的targetMethod方法，结束！");
+    }
+}
